3fc5f09da58f115fd9ddddd417f56a9ac1c4f817
Tala Chek Ateia <tala.ateia@mail.utoronto>
2025-09-08 19:39:12 -0400

    completed task 4

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int count = 0;
+        while (i < 1000) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+            i++;
+            System.out.println(count);
+        }
+    }
+}
