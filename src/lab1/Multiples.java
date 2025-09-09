0817221f9b50976acffb5008cdcd780b5007a96a
Tala Chek Ateia <tala.ateia@mail.utoronto>
2025-09-08 20:06:40 -0400

    completed task 4.1

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,21 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
         int i = 1;
         int count = 0;
-        while (i < 1000) {
-            if (i % 3 == 0 || i % 5 == 0) {
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
                 count++;
             }
             i++;
-            System.out.println(count);
+            ;
+
         }
+        return count;
     }
 }
